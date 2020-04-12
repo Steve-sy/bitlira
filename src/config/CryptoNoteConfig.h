e477ddc5feb1bcb7d24f3553c3596b8786a37141
Steve R <mostafa922@gmail.com>
2020-04-12 21:54:36 +0300

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -374,20 +374,15 @@ namespace CryptoNote
     const uint32_t DATABASE_DEFAULT_MAX_OPEN_FILES = 125; // 125 files
     const uint16_t DATABASE_DEFAULT_BACKGROUND_THREADS_COUNT = 4; // 4 DB threads
 
-    const char LATEST_VERSION_URL[] = "http://latest.turtlecoin.lol";
+    const char LATEST_VERSION_URL[] = "http://latest.turtlecoin.lolZ";
 
-    const std::string LICENSE_URL = "https://github.com/turtlecoin/turtlecoin/blob/master/LICENSE";
+    const std::string LICENSE_URL = "https://github.com/turtlecoin/turtlecoin/blob/master/LICENSEz";
 
     const static boost::uuids::uuid CRYPTONOTE_NETWORK = {
-        {0xb5, 0x0c, 0x4a, 0x6c, 0xcf, 0x52, 0x57, 0x41, 0x65, 0xf9, 0x91, 0xa4, 0xb6, 0xc1, 0x43, 0xe9}};
+        {0xb5, 0x0c, 0x4a, 0x8c, 0xcf, 0x92, 0x47, 0x11, 0x65, 0xf9, 0x91, 0xa4, 0xb6, 0xc7, 0x43, 0xe9}};
 
     const char *const SEED_NODES[] = {
-        "206.189.142.142:11897", // rock
-        "145.239.88.119:11897", // cision
-        "165.227.252.132:11897", // iburnmycd
-        "148.251.178.238:11897", // hv
-        "45.32.138.7:11897", // extra
-        "46.214.70.196:11897", // CuveeRO
-        "94.113.119.122:11897" // CuveeCZ
+        "185.127.18.199:11897", // rock
+        "185.237.98.42:11897", // cision
     };
 } // namespace CryptoNote
